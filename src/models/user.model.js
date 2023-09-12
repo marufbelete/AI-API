0f369d1be3a7d34e8c9cb39a45e21e2a4008c979
marufbelete9@gmail.com <marufbelete9@gmail.com>
2023-09-12 20:42:25 +0300

    cors added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const sequelize = require("../util/database");
 
-const User = sequelize.define("user", {
+const User = sequelize.define("usert", {
   id: {
     type: Sequelize.UUID,
     defaultValue:Sequelize.UUIDV4,
